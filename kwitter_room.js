f483c841a7482d6021fb25caa4b6d8b0a6461c4a
Camera124 <75793966+Camera124@users.noreply.github.com>
2021-01-26 19:12:21 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB8HbcadRtSrumM0COVQ1o2P9R1zRfIbao",
+    authDomain: "project-94-a9df1.firebaseapp.com",
+    databaseURL: "https://project-94-a9df1-default-rtdb.firebaseio.com",
+    projectId: "project-94-a9df1",
+    storageBucket: "project-94-a9df1.appspot.com",
+    messagingSenderId: "1092143166472",
+    appId: "1:1092143166472:web:0e5d79973ac6f7dfb40dbd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
